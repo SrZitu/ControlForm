85ded94c1b573c53cb732d33d1f7aac2a8a34e89
SrZitu <zitu094@gmail.com>
2020-08-13 12:55:30 +0600

    First commit

---
+++ new/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Form from "../src/mycomponent/Form";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
+}
+export default App;
